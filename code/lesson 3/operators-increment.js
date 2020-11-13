5ebcca2a4b82dfaa033a6679c63b356de5f82d09
Paul Anton <paul.antton@gmail.com>
2020-11-13 17:53:50 +0100

    Lesson 3

---
+++ code/lesson 3/operators-increment.js
@@ -0,0 +1,21 @@
+let x = 5;
+
+// Increment Operator ++
+// All these are equivalent
+x=x+1;
+console.log(x);
+x++;
+console.log(x);
+x+=1;
+console.log(x);
+
+console.log("#####")
+
+// Decrement Operator --
+// And so are these
+x=x-1;
+console.log(x);
+x--;
+console.log(x);
+x-=1;
+console.log(x);
---
+++ code/lesson 3/while-loops.js
@@ -0,0 +1,17 @@
+let i = 0;
+
+// while (i<10){
+//     console.log("I am printing the following:" + i);
+//     i++;
+// }
+
+let sum = 0;
+while (i<10){
+    sum +=i;
+    console.log("The sum is:" + sum);
+    i++;
+}
+
+// for (i=0; i<10; i++) {
+//     console.log(i);
+// }
