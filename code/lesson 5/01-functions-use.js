b6f000538e1db0230687d3d7cebd6ac7660f0f92
Paul Anton <paul.antton@gmail.com>
2020-11-18 21:28:48 +0100

    Code for lesson 5

---
+++ code/lesson 5/01-functions-use.js
@@ -0,0 +1,17 @@
+
+let greeting = "Hello there!"
+
+console.log("Hello");
+console.log(greeting);
+
+let value = '5';
+let valueNumber = Number(value);
+// let valueNumber = Number('5');
+
+
+let values =[15, 13];
+let thirdValue = 11;
+
+values.push(11);
+values.push(thirdValue);
+
---
+++ code/lesson 5/05-builtin-functions.js
@@ -0,0 +1,7 @@
+let name = "Paul";
+
+let lowerCase = name.toUpperCase();
+
+console.log(lowerCase);
+
+// Find one JS string method. Read about it, learn to use it, and show the others.
